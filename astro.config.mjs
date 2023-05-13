c55eeb85938d8685b582b3f6193e10612c4ba3c0
Marcos Franco <marcos.franco@smartway.com.ar>
2023-05-13 20:52:35 -0300

    Add configuration to deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://marcos006-dev.github.io',
+  base: '/sada',
 });
